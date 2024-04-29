4d6ef91d4dfd079add0e2f4fe79dab213de07d77
WillSuo <15070906+WillSuo-Github@users.noreply.github.com>
2024-04-29 16:38:05 +0800

    feat(UI): CustomTableRowView add the override funcs

--- NiceHoverViewExample/CustomTableRowView.swift
+++ NiceHoverViewExample/CustomTableRowView.swift
@@ -8,5 +8,19 @@
 import AppKit
 
 final class CustomTableRowView: NiceHoverTableRowView {
+    override func hoverColor() -> NSColor {
+        return NSColor(white: 0, alpha: 0.1)
+    }
     
+    override func xRadius() -> CGFloat {
+        return 20
+    }
+    
+    override func yRadius() -> CGFloat {
+        return 20
+    }
+    
+    override func hoverRect() -> NSRect {
+        return bounds
+    }
 }

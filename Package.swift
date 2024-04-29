b7ab48dff21486e01ce4439eb89d28f5e7ec1123
WillSuo <15070906+WillSuo-Github@users.noreply.github.com>
2024-04-29 17:16:56 +0800

    feat(UI): attempt update the Package.swift to exclude the example folder

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NiceHoverView"),
-        .testTarget(
-            name: "NiceHoverViewTests",
-            dependencies: ["NiceHoverView"]),
+            name: "NiceHoverView",
+            path: "Sources/NiceHoverView",
+            exclude: ["NiceHoverViewExample"]
+        ),
     ]
 )
